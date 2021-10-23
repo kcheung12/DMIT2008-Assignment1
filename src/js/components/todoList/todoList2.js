0b5ad4949c54fc501f3cd9b8cf91038d7b331286
Kai Fai Cheung <kcheung12@nait.ca>
2021-10-23 15:47:00 -0600

    testing the edit/del

--- src/js/components/todoList/todoList2.js
+++ src/js/components/todoList/todoList2.js
@@ -4,6 +4,10 @@ import editlogo from "../../icon/editlogo";
 import trashlogo from "../../icon/trashlogo";
 import { getStore } from "../../redux/store";
 
+const onRequestNewPage = function(e){
+    e.preventDefault();
+    Router(e.currentTarget.dataset.path)
+}
 
 const bgColor = function () {
 
@@ -26,6 +30,10 @@ const todoList = function(){
     elements.forEach(element => {
         content.append(element);
     })
+    const edit = content.querySelector('.edit-button')
+    edit.addEventListener('click',onRequestNewPage)
+    const del = content.querySelector('.edit-button')
+    del.addEventListener('click',onRequestNewPage)
     // data.forEach(cat => {
         
     //     const headDiv = document.createElement('div')
