9565f45ef9ef5c741544cfe75426db067fb22b6b
Kai Fai Cheung <kcheung12@nait.ca>
2021-10-23 16:13:28 -0600

    finding again

--- src/js/pages/toDoPage.js
+++ src/js/pages/toDoPage.js
@@ -31,9 +31,9 @@ const toDoPage = function(){
     
     const category = todoList() 
     main.append(category)
-    const edit = main.querySelector('.edit-button')
+    const edit = main.querySelector('a.edit-button')
     edit.addEventListener('click',onRequestNewPage)
-    const del = main.querySelector('.delete-button')
+    const del = main.querySelector('a.delete-button')
     del.addEventListener('click',onRequestNewPage)
     
     
