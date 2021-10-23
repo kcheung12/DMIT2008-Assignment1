1dfa243f6eb6506964d03722ac6475a457646013
Kai Fai Cheung <kcheung12@nait.ca>
2021-10-23 17:14:38 -0600

    finsihed the assignment

--- src/js/components/todoItem/todoItem.js
+++ src/js/components/todoItem/todoItem.js
@@ -1,14 +1,23 @@
 import makeElement from "../../utils/makeElement"
 
-const todoItem = function({id,category,title,endDate,isComplete}){
+const todoItem = function(color,edit,del,{id,category,title,endDate,isComplete}){
     const template = 
     `
-    <ul class="list" data-key="${id}">
-        <li class="category" >${category}</li>
-        <li class="title" >${title}</li>
-        <li class="date" >${endDate}</li>
-        <li class="isComplete" >${isComplete? 'Completed' : '&nbsp;'}</li>
-    </ul>
+    <div class="itemList">
+        <div class="colorDiv" style="background: ${color};"></div>
+        <div class="catDiv">
+            <ul class="list" data-key="${id}">
+                <li class="category" >${category}</li>
+                <li class="title" >${title}</li>
+                <li class="date" >${endDate}</li>
+                <li class="isComplete" >${isComplete? 'Completed' : '&nbsp;'}</li>
+            </ul>
+            <div class="function-div">
+                <a href="/edit" data-path="/edit" class="edit-button">${edit}</a>
+                <a href="/delete" data-path="/delete" class="delete-button">${del}</a>
+            </div>       
+        </div>
+    </div>
     `
 
     return makeElement(template)
