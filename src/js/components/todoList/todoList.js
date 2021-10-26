4721c01d7979cc3236f81237050ed9962b0cbf55
Kai Fai Cheung <kcheung12@nait.ca>
2021-10-25 19:34:27 -0600

    upload the netlify.toml

--- src/js/components/todoList/todoList.js
+++ src/js/components/todoList/todoList.js
@@ -10,10 +10,10 @@ const onRequestNewPage = function(e){
 }
 const bgColor = function () {
 
-    var x = Math.floor(Math.random() * 256);
-    var y = Math.floor(Math.random() * 256);
-    var z = Math.floor(Math.random() * 256);
-    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+    let x = Math.floor(Math.random() * 256);
+    let y = Math.floor(Math.random() * 256);
+    let z = Math.floor(Math.random() * 256);
+    let bgColor = "rgb(" + x + "," + y + "," + z + ")";
     return bgColor;
 }
 
